904588ff866a558e44a26729e36a3cf093b30f50
Michael D. Hoyle <michael.hoyle@filemobile.com>
2014-07-19 14:45:09 -0400

    starting sessions

--- game/views.py
+++ game/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from game.models import Scene, Option
+from game.models import Scene, Option, Item, Session, Inventory
 
 def index(request):
     context = {}
     return render(request, 'game/index.html', context)
 
 def scene(request, scene_id):
+    normalized_id = int(scene_id)
+    session = ""
+
+    if normalized_id == 1:
+        # start a new session
+        session = "new"
+
+
+
     context = {
-        'scene': Scene.objects.get(pk=int(scene_id)),
-        'options': Option.objects.filter(scene=int(scene_id))
+        'scene': Scene.objects.get(pk=normalized_id),
+        'options': Option.objects.filter(scene=normalized_id),
+        'session': session_id,
     }
     return render(request, 'game/scene.html', context)
 
