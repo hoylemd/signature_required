bfd04031b6e3171c561c14347d96e2a86dbef46d
Michael D. Hoyle <michael.hoyle@filemobile.com>
2014-07-19 11:14:25 -0400

    added admin site

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from game.models import Scene, Option
+
+# custom model admins
+class OptionInline(admin.TabularInline):
+    model = Option
+    extra = 3
+    fk_name = 'scene'
+
+
+class SceneAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Display', {'fields':['title', 'text']}),
+    ]
+    inlines = [OptionInline]
 
 # Register your models here.
+admin.site.register(Scene, SceneAdmin)
