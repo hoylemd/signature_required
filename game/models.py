75f2424583092275cdb23efaedbd87d2c44fad20
Michael D. Hoyle <michael.hoyle@filemobile.com>
2014-07-19 10:49:08 -0400

    Added game app

---
+++ game/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Scene(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=1000)
+    def __unicode__(self):  # Python 3: def __str__(self):
+            return self.title
+
+class Option(models.Model):
+    text = models.CharField(max_length=1000)
+    scene = models.ForeignKey(Scene, related_name="scenes_options")
+    destination = models.ForeignKey(Scene, related_name="option_destinations")
+    change_text_string = models.CharField(max_length=1000)
+    def __unicode__(self):  # Python 3: def __str__(self):
+            return (self.text[:13] + '...') if len(self.text) > 16 else self.text
